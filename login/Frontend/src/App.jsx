6dfcf6ff20e7dd69773c885880f9718743c8948a
Rishav Kumar <rishavkumar18902@gmail.com>
2025-01-25 17:34:56 +0530

    Main.jsx file addec

--- login/Frontend/src/App.jsx
+++ login/Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./components/Home";
+import Main from "./components/Main";
+
 
 
 const App = () => {
@@ -12,6 +14,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
    </div>
---
+++ login/Frontend/src/components/Main.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const main = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-100">Hello</h1>
+    </div>
+  )
+}
+
+export default main
+
