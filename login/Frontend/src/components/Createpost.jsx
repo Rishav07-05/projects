94a9ff2a24a7c3ba2b50d51fdb84bcb305c55dc2
Rishav Kumar <rishavkumar18902@gmail.com>
2025-01-29 10:57:06 +0530

    Create post updated

--- login/Frontend/src/components/Createpost.jsx
+++ login/Frontend/src/components/Createpost.jsx
@@ -1,9 +1,21 @@
 // import React from 'react'
 
+
 const Createpost = () => {
+
+  const [isblurred, setIsblurred] = useState(false)
+
+  const handleClick = () => {
+    setIsblurred(true);
+  }
+
+
   return (
-    <div className='text-center text-2xl text-amber-100'>
-      hello
+    <div className='h-screen w-full flex justify-center items-center'>
+      <div className='flex justify-center items-center gap-10'>
+        <h1 className="text-4xl font-bold text-emerald-500">You can create your post here</h1>
+        <img onClick={handleClick} className={`w-[50px] h-[50px] ${isblurred? "blur-sm opacity-50": "blur-none opacity-100"}`} src="https://www.svgrepo.com/show/13691/plus.svg" alt="" />
+      </div>
     </div>
   )
 }
