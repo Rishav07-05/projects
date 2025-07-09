4f47472164f04db62ed4e99b6ba40e01d9dd3957
Rishav Kumar <rishavkumar18902@gmail.com>
2025-07-09 12:04:41 +0530

    Fix route issue

--- postify/Frontend/src/App.tsx
+++ postify/Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Page from "./Page";
 import Dashboard from "./Dashboard";
 import Share from "./components/Share";
@@ -9,13 +9,15 @@ import Mypost from "./components/Mypost";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Page />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/share" element={<Share />} />
-      <Route path="/interact" element={<Interact />} />
-      <Route path="/mypost" element={<Mypost />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Page />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/share" element={<Share />} />
+        <Route path="/interact" element={<Interact />} />
+        <Route path="/mypost" element={<Mypost />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
