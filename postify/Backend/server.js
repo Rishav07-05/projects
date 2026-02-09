ee748c144b8cfcdbb437c51e7548126f81bcb0ac
Rishav07-05 <rishavkumar18902@gmail.com>
2026-02-09 11:46:03 +0530

    Changes in .env

--- postify/Backend/server.js
+++ postify/Backend/server.js
@@ -7,7 +7,14 @@ import postRoutes from "./routes/posts.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://postify-be.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api", postRoutes);
